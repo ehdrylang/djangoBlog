cba9353ffc8ef7ed5b86937448fa7226268338ac
DESKTOP-TVKLTT8\jdk <ehdrylang@naver.com>
2017-04-22 12:30:22 +0900

    댓글 완성

--- ch3/blogproject/settings.py
+++ ch3/blogproject/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'tagging.apps.TaggingConfig',
+    'disqus',
+    'django.contrib.sites',
 ]
+DISQUS_WEBSITE_SHORTNAME = 'helloprogramming-1'
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'blogproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
